cf2f84d52615901afb3a13a350e355af4ddcec9e
polin <polinataran26@gmail.com>
2022-10-09 20:26:25 +0300

    teach html

--- src/main/java/com/example/web/controllers/RegistrationController.java
+++ src/main/java/com/example/web/controllers/RegistrationController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MainController {
-    @GetMapping("/")
-    public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+public class RegistrationController {
+    @GetMapping("/registration")
+    public String home(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "home";
+        return "registration";
     }
 }
